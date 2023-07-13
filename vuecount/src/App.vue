e34c8fa5b150412e42b3f18c30b68ab81e1d4f20
E-musik <emmamusik888@gmail.com>
2023-07-13 18:28:41 +0100

    VueCount

---
+++ vuecount/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue'
+const count = ref(0)
+</script>
+
+<template>
+  <div>
+    <h4>The current count = </h4>
+    <h1>{{count}}</h1>
+    <button @click="count++">+</button>
+    <button @click="count--">-</button>
+
+  </div>
+</template>
+
+<style scoped>
+button {
+  font-weight: bold;
+}
+h1{
+  color: blue;
+}
+</style>
